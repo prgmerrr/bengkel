5c133d493b4908555e386305365003d18af99255
jarapa <94729750+jarapa@users.noreply.github.com>
2023-09-19 18:20:46 +0700

    w

--- index.php
+++ index.php
@@ -4,13 +4,17 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Halaman Utama</title>
 </head>
 
 <body>
     <?php
-    require "nav.php";
+    // Include navbar.php untuk menampilkan menu navigasi
+    include "navbar.php";
     ?>
+
+    <h1>Selamat datang di Halaman Utama</h1>
+    <p>Ini adalah konten dari halaman utama Anda.</p>
 </body>
 
 </html>
