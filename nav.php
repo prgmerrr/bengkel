e646592068c124b328bfbf90139c917936c8e1b9
jarapa <94729750+jarapa@users.noreply.github.com>
2023-09-27 22:37:58 +0700

    Delete nav.php

--- nav.php
+++
@@ -1,8 +0,0 @@
-<nav>
-    <ul>
-        <li><a href="index.php">Beranda</a></li>
-        <li><a href="about.php">Tentang Kami</a></li>
-        <li><a href="services.php">Layanan</a></li>
-        <li><a href="contact.php">Hubungi Kami</a></li>
-    </ul>
-</nav>
